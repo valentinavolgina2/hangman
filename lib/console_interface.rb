80b344ada55f744a62e4f275d8946d1d5b7a6dca
valentinavolgina2 <64236396+valentinavolgina2@users.noreply.github.com>
2022-03-08 13:25:15 -0800

    Initial commit

---
+++ lib/console_interface.rb
@@ -0,0 +1,60 @@
+class ConsoleInterface
+  # В константе FIGURES будут лежать все текстовые файлы из папки figures,
+  # помещённые в массив. Один элемент массива — одна строка с содержимым целого
+  # файла.
+  FIGURES =
+    Dir["#{__dir__}/../data/figures/*.txt"]
+    .sort
+    .map { |file_name| File.read(file_name) }
+
+  # На вход конструктор класса ConsoleInterface принимает экземпляр класса Game.
+  #
+  # Экземпляр ConsoleInterface выводит информацию юзеру. При выводе использует
+  # статические строки типа "У вас осталось ошибок:" и информацию из экземпляра
+  # класса Game, дёргая у него методы, которые мы придумали.
+  def initialize(game)
+    @game = game
+  end
+
+  def print_out
+    puts <<~END
+      Слово: #{word_to_show}
+      #{figure}
+      Ошибки (#{@game.errors_made}): #{errors_to_show}
+      У вас осталось ошибок: #{@game.errors_allowed}
+
+    END
+
+    if @game.won?
+      puts 'Поздравляем, вы выиграли!'
+    elsif @game.lost?
+      puts "Вы проиграли, загаданное слово: #{@game.word}"
+    end
+  end
+
+  def figure
+    FIGURES[@game.errors_made]
+  end
+
+  def word_to_show
+    result =
+      @game.letters_to_guess.map do |letter|
+        if letter.nil?
+          '__'
+        else
+          letter
+        end
+      end
+
+    result.join(' ')
+  end
+
+  def errors_to_show
+    @game.errors.join(', ')
+  end
+
+  def get_input
+    print 'Введите следующую букву: '
+    gets[0].upcase
+  end
+end
---
+++ main.rb
@@ -0,0 +1,29 @@
+if Gem.win_platform?
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+require_relative 'lib/console_interface'
+require_relative 'lib/game'
+
+# 1. Поздороваться
+puts 'Всем привет!'
+
+# 2. Загрузить случайное слово из файла
+word = File.readlines("#{__dir__}/data/words.txt", encoding: 'UTF-8', chomp: true).sample
+game = Game.new(word)
+console_interface = ConsoleInterface.new(game)
+
+# 3. Пока не закончилась игра
+until game.over?
+  console_interface.print_out
+  letter = console_interface.get_input
+  game.play!(letter)
+end
+
+# 4. Вывести финальное состояние игры
+console_interface.print_out
